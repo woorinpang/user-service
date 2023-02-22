891821531d5feafac57b73a4620cf33c0c9dd2c1
hclee <heechul4296@gmail.com>
2023-02-22 22:00:05 +0900

    feature/(#8) - SecurityConfig 추가

--- src/main/java/com/wooringpang/userservice/config/SecurityConfig.java
+++ src/main/java/com/wooringpang/userservice/config/SecurityConfig.java
@@ -1,13 +1,60 @@
 package com.wooringpang.userservice.config;
 
+import com.wooringpang.userservice.domain.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static com.wooringpang.userservice.config.GlobalConstant.*;
 
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
 
+    private final TokenProvider tokenProvider;
+    private final UserService userService;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Bean
+    protected SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .csrf().disable()
+                .headers().frameOptions().disable()
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS) //토큰 사용하기 때문에 세션은 비활성화
+                .and()
+                .authorizeHttpRequests()
+                .requestMatchers(SECURITY_PERMITAIL_ANTPATTERNS).permitAll()
+                .anyRequest().permitAll()
+                .and()
+                .build();
+//                .anyRequest().access("@authorizationService.isAuthorization(request, authentication)") //호출 시 권한 인가 데이터 확인
+//                .and()
+//                .addFilter(getAuthenticationFilter())
+//                .addFilter()
+//                .and()
+//                .logout()
+//                .logoutSuccessUrl("/")
+//                .build();
+    }
+
+//    @Bean
+//    protected AuthenticationFilter getAuthenticationFilter() throws Exception {
+//        return new AuthenticationFilter(authenticationManager, tokenProvider, userService);
+//    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+        return authConfig.getAuthenticationManager();
+    }
 }
