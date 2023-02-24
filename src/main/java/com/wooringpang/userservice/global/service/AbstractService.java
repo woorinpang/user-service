1d22809f67ad50f0032c0d834a82ad8eeef73253
hclee <heechul4296@gmail.com>
2023-02-25 00:21:41 +0900

    [Feature] MessageUtil 및 AbstractService 기능 개발#21 - MessageUtil 기능 - AbstractService 기능

--- src/main/java/com/wooringpang/userservice/global/service/AbstractService.java
+++ src/main/java/com/wooringpang/userservice/global/service/AbstractService.java
@@ -1,6 +1,18 @@
 package com.wooringpang.userservice.global.service;
 
+import com.wooringpang.userservice.global.util.MessageUtil;
+import jakarta.annotation.Resource;
+
 public abstract class AbstractService {
 
+    @Resource(name = "messageUtil")
+    private MessageUtil messageUtil;
 
+    protected String getMessage(String code) {
+        return messageUtil.getMessage(code);
+    }
+
+    private String getMessage(String code, Object[] args) {
+        return messageUtil.getMessage(code, args);
+    }
 }
--- src/main/java/com/wooringpang/userservice/global/util/MessageUtil.java
+++ src/main/java/com/wooringpang/userservice/global/util/MessageUtil.java
@@ -1,9 +1,27 @@
 package com.wooringpang.userservice.global.util;
 
+import jakarta.annotation.Resource;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Component;
+
+import java.util.Locale;
 
 @Component
 public class MessageUtil {
 
+    @Resource(name = "messageSource")
+    private MessageSource messageSource;
 
+    public String getMessage(String code) {
+        return this.getMessage(code, new Object[]{});
+    }
+
+    public String getMessage(String code, Object[] args) {
+        return this.getMessage(code, args, LocaleContextHolder.getLocale());
+    }
+
+    public String getMessage(String code, Object[] args, Locale locale) {
+        return this.getMessage(code, args, locale);
+    }
 }
