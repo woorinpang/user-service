cde0d13f143fa3a76d7ebab5f474e8125bf39ce2
heechul <heechul4296@gmail.com>
2023-11-27 22:06:53 +0900

    [feature : #84] 마이그레이션중

---
+++ core/core-api/src/main/java/io/woorinpang/userservice/core/api/HealthController.java
@@ -0,0 +1,14 @@
+package io.woorinpang.userservice.core.api;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+    @GetMapping("/health")
+    public ResponseEntity health() {
+        return ResponseEntity.status(HttpStatus.OK).body("user-service");
+    }
+}
